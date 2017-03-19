29ad7c2f3d408fb4a55aecf31fa089b9cf3c5a52
Katarina1330 <dordevic.katarina@outlook.com>
2017-03-19 07:55:10 -0700

    Implemented message for Delete button and added small corrections

--- ToDo/JavaScript/App.js
+++ ToDo/JavaScript/App.js
@@ -1,7 +1,7 @@
 ﻿
 // Get All Tasks
 var taskApp = angular.module('TaskApp', []);
-taskApp.controller('taskCtrl',  ['$scope', '$http', function ($scope, $http) {
+taskApp.controller('taskCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.isEditMode = false;
     $scope.toogleEditMode = function (isEditMode) {
         $scope.isEditMode = !isEditMode;
@@ -20,13 +20,27 @@ taskApp.controller('taskCtrl',  ['$scope', '$http', function ($scope, $http) {
     }
 
     $scope.delete = function (task) {
-        $http.post('http://localhost:49878/api/task/delete', task).success(function (data, status, headers, config) {
-            alert("uspelo!");
-        }).error(function (data, status, headers, confing) {
-            alert("error");
-        })
-    }
+        var r = confirm("Do you want to delete your task?");
+        if (r == true) {
+            $http.post('http://localhost:49878/api/task/delete', task).success(function (data, status, headers, config) {
 
+                $http.get('http://localhost:49878/api/task/getalltasks').success(function (data, status, headers, config) {
+                    $scope.tasks = data;
+                }).error(function (data, status, headers, config) {
+                    alert("error");
+                });
+            }).error(function (data, status, headers, confing) {
+                alert("error");
+            })
+        } else {
+            $http.get('http://localhost:49878/api/task/getalltasks').success(function (data, status, headers, config) {
+                $scope.tasks = data;
+
+            }).error(function (data, status, headers, config) {
+                alert("error");
+            });
+        }
+    }
 }]);
 
 // Create new Task
@@ -36,8 +50,10 @@ taskAdd.controller('TaskAddController', ['$scope', '$http', function ($scope, $h
         if ($scope.Title) {
             var task = {
                 "Title": $scope.Title,
+                "CreatedDate": $scope.CreatedDate,
                 "Description": $scope.Description,
-                "State": $scope.State
+                "State": $scope.State,
+                "DeadlineDate": $scope.DeadlineDate
             }
             $http.post('http://localhost:49878/api/task/add', task).success(function (data, status, headers, config) {
             }).error(function (data, status, headers, confing) {
